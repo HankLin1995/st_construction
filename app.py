0a0d694f869b24d0f14a22dce9f3af7d20bc1bf8
Hank <apple84026113@gmail.com>
2024-07-16 20:55:39 +0800

    change page

---
+++ app.py
@@ -0,0 +1,22 @@
+import streamlit as st
+
+## session_state init
+
+if 'user_id' not in st.session_state:
+    st.session_state.user_id=""
+
+## navigation
+
+# st.logo("logo.jpg")
+
+p1=st.Page("views/information.py",title="基本資料",icon=":material/home:")
+p2=st.Page("views/contract.py",title="契約項目",icon=":material/add:")
+p3=st.Page("views/progress.py",title="進度設定",icon=":material/open_in_new:")
+
+
+pg=st.navigation({
+    "主要功能":[p1],
+    "專案":[p2,p3]
+})
+
+pg.run()
